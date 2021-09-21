f6bdc9ef1a0c4353e9f6d6e680c5999e2f63957a
dobby <dobby@diilabs.co.kr>
2021-09-21 19:26:50 +0900

    Initial commit

---
+++ src/main/java/com/example/cricketstory/CricketstoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.cricketstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketstoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CricketstoryApplication.class, args);
+    }
+
+}
